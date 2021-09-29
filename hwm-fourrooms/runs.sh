d9d9edd500ac956393487b5ff8e64caedf2d1add
dosssman <dosssman@hotmail.fr>
2021-09-29 16:43:10 +0900

    added utils to generate FourRoom dataset, as well as padding of the dataset and other preprocessing

---
+++ hwm-fourrooms/runs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+NUM_CORES=$(nproc --all)
+export MKL_NUM_THREADS=$NUM_CORES OMP_NUM_THREADS=$NUM_CORES
+
+# export WANDB_DIR=~/random/rl/hwm-logs/hwm_minigrid_2
+# if [ ! -d $WANDB_DIR ]; then
+#   mkdir -p $WANDB_DIR
+# fi
+
+############################
+# region: PPO Baselines FourRoom task (custom)
+############################
+export CUDA_VISIBLE_DEVICES=6
+(sleep 1s && nohup xvfb-run -a python ppo_fact_state_4rooms.py) >& /dev/null &
+(sleep 1s && nohup xvfb-run -a python ppo_full_grid_obs_4rooms.py ) >& /dev/null &
+############################
+# endregion: PPO Baselines FourRoom task (custom)
+############################
+
+export CUDA_VISIBLE_DEVICES=
+
+export WANDB_DIR=
